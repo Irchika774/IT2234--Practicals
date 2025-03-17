226dae0bff9e553f06c36033322b1663a7b8d98d
Irchika774 <waruirchika@gmail.com>
2025-03-17 19:15:05 +0530

    Daily practical update

---
+++ Practical-01/Basic.js
@@ -0,0 +1,71 @@
+/*console.log("hello")
+
+let x=5;
+let y=8;
+
+var a =1;
+var b=2;
+
+console.log(x+y);
+console.log(x*y);
+console.log(x*y);
+console.log(y/x);
+console.log(a+b);
+
+
+//numbers from 1 to 10
+
+for(let i=0;i<=10;i++)
+{
+	console.log(i);
+}
+
+//print odd numbers
+
+for(let i=0;i<=10;i++)
+{
+	
+	if(i%2==1)
+	{
+		console.log(i);
+	}
+}
+// print the numbers in reverse
+
+for(let i=10;i>=0;i--)
+{
+	console.log(i);
+}
+*/
+
+
+//reverse the  numbers in left and right 4321-5-9876
+
+
+let num=9;
+let mid=(num+1)/2;
+
+
+		for(let j=mid-1;j>=1;j--)
+		{
+			console.log(j);
+		}
+		console.log(mid);
+		for(let m=num;m>=mid+1;m--)
+		{
+			console.log(m);
+		}
+		
+		
+	
+	
+	
+	
+
+	
+
+
+
+
+
+ 
