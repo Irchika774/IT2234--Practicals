ef9a1f255bd3f7283a2f9aa4d628cf8fc3715eb0
Irchika774 <waruirchika@gmail.com>
2025-04-18 22:44:22 +0530

    First commit using batch file

--- update_git.bat
+++ update_git.bat
@@ -1,9 +1,7 @@
 @echo off
 cd C:\Users\Warushi\Desktop\VLE\2.2\IT2234--Practicals
-git init
-git remote add origin https://github.com/Irchika774/IT2234--Practicals.git
-git pull origin master
+git pull origin main
 git add .
 git commit -m "First commit using batch file"
-git push -u origin master
+git push origin main
 pause
